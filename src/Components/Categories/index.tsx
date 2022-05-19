47cf8cf94b568c076591ed99f2082eb72283273e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-05-18 22:30:39 -0300

    remove unecessary import

--- src/Components/Categories/index.tsx
+++ src/Components/Categories/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { SelectedCategory } from "../../Contexts/SelectedCategory";
 
 export const Categories = () => {
