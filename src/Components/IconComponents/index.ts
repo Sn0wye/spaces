0bcd6c41ed9295f61f457c8bdcc786d65076db51
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-02 17:28:29 -0300

    💄 Create Icon Components

---
+++ src/Components/IconComponents/index.ts
@@ -0,0 +1,5 @@
+export { default as Email } from "./Email";
+export { default as EyeVisible } from "./EyeVisible";
+export { default as Google } from "./Google";
+export { default as Password } from "./Password";
+export { default as User } from "./User";
