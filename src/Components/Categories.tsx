218936e15a430b7047e43afb5d71cdc7a40b3595
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-07 19:30:41 -0300

    ♻️  Refactor Dynamic Categories

--- src/Components/Categories.tsx
+++ src/Components/Categories.tsx
@@ -1,56 +1,23 @@
 import { useContext } from "react";
 import { SelectedCategory } from "../Contexts/SelectedCategory";
+import { Category } from "./Category";
+
+let categories = {
+  "": "All",
+  Work: "Work",
+  Home: "Home",
+  Personal: "Personal",
+  Shopping: "Shopping",
+};
 
 export const Categories = () => {
-  const { selectedCategory, setSelectedCategory } =
-    useContext(SelectedCategory);
-
-  function handleSetSelected(newCategory: string) {
-    setSelectedCategory(newCategory);
-  }
+  useContext(SelectedCategory);
 
   return (
     <section className="w-fit flex flex-col gap-4 text-3xl px-12 pt-44 ">
-      <h1
-        className={
-          selectedCategory === "all"
-            ? "font-bold text-brand p-2"
-            : "cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 rounded-lg transition-colors p-2"
-        }
-        onClick={() => handleSetSelected("all")}
-      >
-        All
-      </h1>
-      <h1
-        className={
-          selectedCategory === "groceries"
-            ? "font-bold text-brand p-2"
-            : "cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 rounded-lg transition-colors p-2"
-        }
-        onClick={() => handleSetSelected("groceries")}
-      >
-        Groceries
-      </h1>
-      <h1
-        className={
-          selectedCategory === "college"
-            ? "font-bold text-brand p-2"
-            : "cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 rounded-lg transition-colors p-2"
-        }
-        onClick={() => handleSetSelected("college")}
-      >
-        College
-      </h1>
-      <h1
-        className={
-          selectedCategory === "payments"
-            ? "font-bold text-brand p-2"
-            : "cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 rounded-lg transition-colors p-2"
-        }
-        onClick={() => handleSetSelected("payments")}
-      >
-        Payments
-      </h1>
+      {Object.entries(categories).map(([key, value]) => (
+        <Category key={key}>{value}</Category>
+      ))}
     </section>
   );
 };
---
+++ src/Components/Category.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useContext } from "react";
+import { SelectedCategory } from "../Contexts/SelectedCategory";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Category = ({ children }: Props) => {
+  const { selectedCategory, setSelectedCategory } =
+    useContext(SelectedCategory);
+  return (
+    <h1
+      className={
+        selectedCategory === ""
+          ? "font-bold text-brand p-2"
+          : "cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 rounded-lg transition-colors p-2"
+      }
+      onClick={() => setSelectedCategory(`${children}`)}
+    >
+      {children}
+    </h1>
+  );
+};
