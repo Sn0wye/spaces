e7d7f9f7998eb4fe66c15274f7757ceb33bc40d9
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-08 16:44:18 -0300

    ➕  Add Supabase and its configuration file

---
+++ src/Services/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
