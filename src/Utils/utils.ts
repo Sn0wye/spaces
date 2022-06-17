4cf46f33cc473e6c19966f9c080bf811dd96740b
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-17 12:49:37 -0300

    ✨ Add filter for removing duplicate array elements

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -2,4 +2,14 @@ function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export { capitalizeFirstLetter };
+function removeArrayElementDuplicates(array: any[], prop: string) {
+  const uniqueIds = new Set();
+  const unique = array.filter((element) => {
+    const isDuplicate = uniqueIds.has(element[prop]);
+    uniqueIds.add(element[prop]);
+    return !isDuplicate;
+  });
+  return unique;
+}
+
+export { capitalizeFirstLetter, removeArrayElementDuplicates };
