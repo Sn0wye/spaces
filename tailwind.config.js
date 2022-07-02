1138ab5f38a012f333b6f3a5b32e5c30ceb96f43
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-02 19:45:44 -0300

    ✨ Add new colors to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,43 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  mode: "jit",
-  darkMode: "class", // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: 'jit',
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        brand: "#8257e6",
-        "brand-light": "#996DFF",
+        brand: {
+          600: '#5C4BC5',
+          500: '#8257e6',
+          400: '#5E60CE',
+          300: '#996DFF',
+        },
+        black: '#0D0D0D',
+        danger: '#e25858',
+        surface: {
+          dark: '#1A1A1A',
+          light: '#FFF',
+        },
+        text: {
+          dark: '#f4f4f5',
+          light: '#27272a',
+        },
+        gray: {
+          700: '#0D0D0D',
+          600: '#1A1A1A',
+          500: '#262626',
+          400: '#333333',
+          300: '#808080',
+          200: '#d9d9d9',
+          100: '#f2f2f2',
+        },
       },
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        inter: ['Inter', 'sans-serif'],
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
