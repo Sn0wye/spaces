9a8fb20a76580346a3e5e1370d49370e0fee4bef
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-11 16:42:53 -0300

    💄 Update modal style

--- src/components/Modals/EditTodoModal.tsx
+++ src/components/Modals/EditTodoModal.tsx
@@ -40,19 +40,19 @@ const EditTodoModal = ({
           leaveFrom='opacity-100 scale-100'
           leaveTo='opacity-0 scale-95'
         >
-          <Dialog.Panel className='w-full h-fit max-w-sm bg-white dark:bg-zinc-800 rounded-md p-4 flex flex-col justify-center items-center'>
-            <Dialog.Title className='font-bold text-black dark:text-white text-2xl'>
+          <Dialog.Panel className='w-full h-fit max-w-sm bg-white dark:bg-zinc-800 rounded-md p-3 flex flex-col justify-center items-center'>
+            <Dialog.Title className='font-bold text-black dark:text-white text-2xl mt-2'>
               Update Todo Info
             </Dialog.Title>
             <form className=' w-full flex flex-col justify-center items-center'>
               <textarea
                 value={newTodoDescription}
                 onChange={(e) => setNewTodoDescription(e.target.value)}
-                className='w-4/5 p-2 mt-8 font-inter text-md font-normal text-zinc-500 dark:text-zinc-400 border-none outline-none bg-zinc-100 dark:bg-zinc-700 rounded-xl focus:border-brand-500 focus:outline-brand-500 resize-x min-h-[6rem] '
+                className='w-full p-2 mt-8 font-inter text-md font-normal text-zinc-500 dark:text-zinc-400 border-none outline-none bg-zinc-100 dark:bg-zinc-700 rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none border-white dark:border-none resize-y min-h-[6rem] '
               />
               <button
                 type='submit'
-                className='text-white font-bold rounded-xl bg-brand-500 p-4 w-20 h-10 flex justify-center items-center mt-8 hover:bg-brand-300 transition-colors shadow-md'
+                className='text-white font-bold rounded-md bg-brand-500 p-4 w-full h-10 flex justify-center items-center mt-8 hover:bg-brand-300 transition-colors shadow-md'
                 onClick={() => {
                   handleToggleModal();
                   handleUpdateTodo(todo.id, newTodoDescription);
